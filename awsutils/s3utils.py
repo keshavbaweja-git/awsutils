df137695d4ca32669feccb1470ee102759dd1086
Keshav Baweja <keshavbaweja@gmail.com>
2021-08-31 16:21:42 +0800

    Fixed bucket check, delete operations

--- awsutils/s3utils.py
+++ awsutils/s3utils.py
@@ -8,19 +8,17 @@ s3 = boto3.resource('s3')
 @click.command()
 @click.option('--bucket_name', help='bucket name')
 def s3_delete_bucket(bucket_name):
-    if not bucket_exists(bucket_name):
+    bucket = s3.Bucket(bucket_name)
+    if not bucket_exists(bucket):
         print("Bucket %s does not exist" % (bucket_name))
         return
-
     print("Deleting bucket %s" % (bucket_name))
-    bucket = s3.Bucket(bucket_name)
-    bucket.object_version.all().delete()
+    bucket.object_versions.all().delete()
     bucket.delete()
 
 
-def bucket_exists(bucket_name):
-    try:
-        s3.meta.client.head_bucket(Bucket=bucket_name)
+def bucket_exists(bucket):
+    if bucket.creation_date:
         return True
-    except ClientError:
+    else:
         return False
